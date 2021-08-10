d2f7147fe7e0eebed21a08f1b60cff4f72aea102
Vansh Mohan <69002944+vanshman@users.noreply.github.com>
2021-08-10 16:11:07 +0530

    nice
    nice

---
+++ main.js
@@ -0,0 +1,14 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(500, 500);
+
+    canvas = createCanvas(500, 450);
+    canvas.position(800, 150);
+
+    
+
+}
+function draw()
+{
+    background("grey");
+}

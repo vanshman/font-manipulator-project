bd8d9d994fabe2ab9f9e6912d23977d586ebf7de
Vansh Mohan <69002944+vanshman@users.noreply.github.com>
2021-08-11 16:23:25 +0530

    nice
    nice

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup(){
     video = createCapture(VIDEO);
     video.size(500, 500);
@@ -5,10 +10,33 @@ function setup(){
     canvas = createCanvas(500, 450);
     canvas.position(800, 150);
 
-    
-
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+function modelLoaded()
+{
+    console.log("posenet initialized");
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+    }
 }
 function draw()
 {
     background("grey");
+
+    document.getElementById("square_side").innerHTML = "";
+    textSize(difference);
+    fill("red");
+    stroke("black");
+    text("Vansh", noseX, noseY);
 }
